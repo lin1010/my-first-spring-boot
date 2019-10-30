0e00ab61a989cb8b0469fb07f2acc375f243cf51
Xiaoling XL4 Lin <linxl4@Lenovo.com>
2019-10-30 17:17:51 +0800

    first commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import com.example.demo.model.AyUser;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.annotation.Resource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Resource
+	private JdbcTemplate jdbcTemplate;
+
+	/**
+	 * Mysql集成Spring Boot简单测试
+	 */
+	@Test
+	public void mySqlTest() {
+		String sql = "select id,name,password from ay_user";
+		List<AyUser> userList =
+				(List<AyUser>) jdbcTemplate.query(sql, new RowMapper<AyUser>() {
+					@Override
+					public AyUser mapRow(ResultSet rs, int rowNum) throws SQLException {
+						AyUser user = new AyUser();
+						user.setId(rs.getLong("id"));
+						user.setName(rs.getString("name"));
+						user.setPassword(rs.getString("password"));
+						return user;
+					}
+				});
+		System.out.println("查询成功：");
+		for (AyUser user : userList) {
+			System.out.println("【id】: " + user.getId() + "；【name】: " + user.getName());
+		}
+	}
+
+}
